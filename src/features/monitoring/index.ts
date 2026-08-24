4ea78b3c781453ee63191a027be79b1f01d77cb3
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-25 01:34:10 +0200

    Export Sentry depuis barrel

--- src/features/monitoring/index.ts
+++ src/features/monitoring/index.ts
@@ -1,3 +1,4 @@
 export { useHealthChecks } from './hooks/useHealthChecks';
+export { initSentry, setSentryUser, clearSentryUser, captureException } from './services/sentryService';
 
 export type * from './types';
