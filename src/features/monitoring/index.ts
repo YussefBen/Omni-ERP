f931609bd0160a0f7f7163a322c359f35e74f7ce
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-26 16:03:12 +0200

    Hook usePageViewTracking (pages vues automatiques)

--- src/features/monitoring/index.ts
+++ src/features/monitoring/index.ts
@@ -2,5 +2,8 @@ export { useHealthChecks } from './hooks/useHealthChecks';
 export { initSentry, setSentryUser, clearSentryUser, captureException } from './services/sentryService';
 export { initWebVitals } from './services/webVitalsService';
 export { useWebVitals } from './hooks/useWebVitals';
+export { initAnalytics, trackEvent } from './services/analyticsService';
++export { useEventTracking } from './hooks/useEventTracking';
++export { usePageViewTracking } from './hooks/usePageViewTracking';
 
 export type * from './types';
