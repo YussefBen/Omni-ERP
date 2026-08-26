4eb8da6822af90616dd6ed485805d35319da9644
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-26 16:46:26 +0200

    Fix error de frappe

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { initSentry } from './features/monitoring'
-import { initSentry, initWebVitals } from './features/monitoring'
-import { initAnalytics, initSentry } from './features/monitoring'
+import { initAnalytics, initSentry, initWebVitals } from './features/monitoring'
 
 initSentry();
 initWebVitals();
