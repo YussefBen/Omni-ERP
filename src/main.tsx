6aa949a96944bd689494600dbecb989c9e5af845
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-26 16:19:40 +0200

    Init GoogleAnalytics au démarrage

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import { initSentry } from './features/monitoring'
 import { initSentry, initWebVitals } from './features/monitoring'
+import { initAnalytics, initSentry } from './features/monitoring'
 
 initSentry();
 initWebVitals();
+initAnalytics();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
