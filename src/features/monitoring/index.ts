d4b3b99a7fe5a3468cc11055b3b72b27a30d4ed1
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-27 15:48:03 +0200

    Hooks useFeatureFlag & useCanaryFeature

--- src/features/monitoring/index.ts
+++ src/features/monitoring/index.ts
@@ -5,5 +5,8 @@ export { useWebVitals } from './hooks/useWebVitals';
 export { initAnalytics, trackEvent } from './services/analyticsService';
 export { useEventTracking } from './hooks/useEventTracking';
 export { usePageViewTracking } from './hooks/usePageViewTracking';
+export { initFeatureFlags } from './services/featureFlagService';
+export { useFeatureFlag } from './hooks/useFeatureFlag';
+export { useCanaryFeature } from './hooks/useCanaryFeature';
 
 export type * from './types';
