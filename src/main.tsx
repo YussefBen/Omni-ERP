e216990746f77759ecddc4c93278cc15daf22fc5
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-27 16:14:01 +0200

    Init Flagmsmith démarrage

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { initAnalytics, initSentry, initWebVitals } from './features/monitoring'
+import { initAnalytics, initFeatureFlags, initSentry, initWebVitals } from './features/monitoring'
 
-initSentry();
-initWebVitals();
-initAnalytics();
+async function bootstrap() {
+  initSentry();
+  initWebVitals();
+  initAnalytics();
+  await initFeatureFlags();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
+bootstrap();
