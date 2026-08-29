757e5caafd4e9d87ae752353e8c289fb90baa345
JessicaD3 <jessica.dubois1003@gmail.com>
2026-08-29 04:22:41 +0200

    fix erreur import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { initAnalytics, initFeatureFlags, initSentry, initWebVitals } from './features/monitoring'
 import { initAnalytics, initFeatureFlags, initSentry, initTracing, initWebVitals } from './features/monitoring'
 
 async function bootstrap() {
