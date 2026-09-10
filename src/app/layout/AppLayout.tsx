17bf6a6248ad1a370c0f7419610479bcca17a011
JessicaD3 <jessica.dubois1003@gmail.com>
2026-09-10 22:17:58 +0200

    Extraction routePreloaders dans son prorpe fichier. Bug sinon

--- src/app/layout/AppLayout.tsx
+++ src/app/layout/AppLayout.tsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 import { useQueryClient } from '@tanstack/react-query';
 import { ThemeToggle } from '@/shared/components/ThemeToggle/ThemeToggle';
 import { useOnlineStatus } from '@/shared/hooks/useOnlineStatus';
-import { routePreloaders } from '../../router';
 import { prefetchRouteData } from './prefetchRouteData';
 import styles from './AppLayout.module.css';
+import { routePreloaders } from '../../routePreloaders';
 
 interface NavItem {
   to: string;
